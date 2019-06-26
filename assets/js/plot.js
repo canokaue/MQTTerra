39ec6eabee333e76b92cf86d2db982e10bda20c5
André Schlichting <andrelucas@linse-093.linse.ufsc.br>
2019-06-26 12:32:59 -0300

    plot measurements history

--- assets/js/plot.js
+++ assets/js/plot.js
@@ -1,7 +1,44 @@
 var originalURL = "https://tranquil-forest-64117.herokuapp.com/waterlevelsjson/";
 var queryURL = "https://cors-anywhere.herokuapp.com/" + originalURL
 
-var resp = [];
+
+function plot(resp) {
+  console.log("plot data");
+  var xaxis = [];
+  var yaxis = [];
+  var i;
+  var traceWL = {
+             x: xaxis,
+             y: yaxis,
+             fill: 'tonexty',
+             type: 'line',
+             mode: 'none'
+         };
+  
+  for (i = 0; i < resp.length; i++) { 
+    //console.log(resp[i].water_level);
+    xaxis.push(resp[i].timestamp);
+    yaxis.push(resp[i].water_level)
+  }
+
+  var traceWL = {
+             x: xaxis,
+             y: yaxis,
+             fill: 'tonexty',
+             type: 'line',
+             mode: 'none'
+         };
+
+  var dataWL = [ traceWL ];
+
+  var layout = {
+    title: 'Measurements History'
+  };
+
+   Plotly.newPlot('plotdiv', dataWL, layout, {responsive: true});
+   //Plotly.newPlot('footer', dataWL, layout);
+}
+
   $.ajax({
       url: queryURL,
       method: "GET",
@@ -11,10 +48,13 @@ var resp = [];
         "x-requested-with": "xhr" 
       }
     }).done(function(response) {
-      resp.push(response);
+      //resp.push(response);
       console.log('CORS anywhere response', response);
+      plot(response);
+      
     }).fail(function(jqXHR, textStatus) { 
       console.error(textStatus)
     })
 
-    console.log("potato",resp);
+
+
